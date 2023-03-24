f91f338f6c5144416c47a154687b40ec19a02b30
Michael <michael@m-davis.com>
2023-03-24 11:22:28 -0700

    it works!

--- popup.js
+++ popup.js
@@ -1,1 +1,29 @@
-document.querySelector('button[aria-label*="microphone"]').click();
+//document.querySelector('button[aria-label*="microphone"]').click();
+
+document.addEventListener('DOMContentLoaded', function () {
+  const muteButton = document.getElementById("mutebutton");
+  console.log(muteButton);
+  muteButton.addEventListener("click", function() {
+  	(async () => {
+	  	console.log("Mute Button Clicked");
+	  	//const tabs = await chrome.tabs.query({title: "Google Meet"});
+	  	const tabs = await chrome.tabs.query({url: "*://*.meet.google.com/*"});
+	  	//console.log(tabs)
+	  	meetTab = tabs[0];
+	  	console.log(meetTab);
+	  	if (meetTab) {
+			console.log(meetTab);
+			chrome.scripting.executeScript({
+				target: {
+					tabId: meetTab.id
+				},
+				func: function () {
+					//console.log("TEST");
+					document.querySelector('button[aria-label*="microphone"]').click();
+				}
+			})
+	  	}
+	  	
+  	})();
+  });
+});
